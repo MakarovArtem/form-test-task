39610e25e4ac14228d0019594287048f956f3d93
Artem <artemmakarov76@yandex.ru>
2023-06-08 15:30:19 +0300

    feat: make tip field for input, make input and button highlights with hovering and focus

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -5,14 +5,16 @@ interface InputProps {
   title: string;
   type: string;
   placeholder?: string;
+  tip?: string;
   id?: string;
 }
 
-const Input: FC<InputProps> = ({title, type, placeholder}) => {
+const Input: FC<InputProps> = ({title, type, placeholder, tip}) => {
   return (
     <div className={style.inputContainer}>
       <p className={style.inputTitle}>{title}</p>
       <input className={style.input} type={type} placeholder={placeholder ? placeholder : 'Placeholder'}/>
+      {tip && <p className={style.inputTip}>{tip}</p>}
     </div>
   )
 }
