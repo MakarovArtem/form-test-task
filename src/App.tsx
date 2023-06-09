07de5d84d87cb756149d6315e370ba01666b4277
Artem <artemmakarov76@yandex.ru>
2023-06-09 16:19:03 +0300

    feat: make dit for form step two page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import MainScreen from "./pages/mainScreen/MainScreen";
 import FormStepOne from "./pages/formStepOne/FormStepOne";
+import FormStepTwo from "./pages/formStepTwo/FormStepTwo";
 
 function App() {
   return (
     <div className="App">
       {/* <MainScreen /> */}
-      <FormStepOne />
+      {/* <FormStepOne /> */}
+      <FormStepTwo />
     </div>
   );
 }
---
+++ src/pages/formStepTwo/FormStepTwo.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+import style from "./FormStepTwo.module.css";
+import ProgressLine from "../../components/progressLine/ProgressLine";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
+
+interface FormStepTwoProps {}
+
+const FormStepTwo: FC<FormStepTwoProps> = () => {
+  return (
+    <article className={style.main}>
+      <div className={style.mainContainer}>
+        <ProgressLine step="three" />
+        <form className={style.form} action="post">
+          <div className={style.nickContainer}>
+            <Input width="300px" title="Nickname" type="text" tip="tip" id="field-nickname"/>
+          </div>
+          <div className={style.nameContainer}>
+            <Input width="300px" title="Name" type="text" tip="tip" id="field-name"/>
+          </div>
+          <div className={style.surnameContainer}>
+            <Input width="300px" title="Surname" type="text" tip="tip" id="field-surname"/>
+          </div>
+          <div className={style.sexContainer}>
+            <Input width="300px" title="Sex" type="select" tip="tip" id="field-sex"/>
+            {/* нужно сделать кастомный селект */}
+          </div>
+          <div className={style.backContainer}>
+            <Button text="Назад" themeBlue={false} id="button-back" />
+          </div>
+          <div className={style.nextContainer}>
+            <Button text="Далее" themeBlue={true} id="button-next" />
+          </div>
+        </form>
+      </div>
+    </article>
+  )
+}
+
+export default FormStepTwo;
