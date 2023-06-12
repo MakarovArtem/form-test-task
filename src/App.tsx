d39eaddcd3f1d6c3ac7a763ca4f28b4eb5136541
Artem <artemmakarov76@yandex.ru>
2023-06-12 18:45:39 +0300

    feat: setup react router, configure starting config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import MainScreen from "./pages/mainScreen/MainScreen";
-import FormStepOne from "./pages/formStepOne/FormStepOne";
-import FormStepTwo from "./pages/formStepTwo/FormStepTwo";
-import FormStepThree from "./pages/formStepThree/FormStepThree";
-import ModalWindow from "./components/modalWindow/ModalWindow";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <MainScreen /> */}
-      {/* <FormStepOne /> */}
-      <FormStepTwo />
-      {/* <FormStepThree /> */}
-      {/* <ModalWindow isSuccessfull={false} /> */}
+      <MainScreen />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import FormStepOne from './pages/formStepOne/FormStepOne';
+import FormStepTwo from './pages/formStepTwo/FormStepTwo';
+import FormStepThree from './pages/formStepThree/FormStepThree';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/form-step-one",
+    element: <FormStepOne />
+  },
+  {
+    path: "/form-step-two",
+    element: <FormStepTwo />
+  },
+  {
+    path: "/form-step-three",
+    element: <FormStepThree />
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
