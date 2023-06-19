cdfb80cce5944262914d6842eb093539429d25fb
Artem <artemmakarov76@yandex.ru>
2023-06-19 05:04:24 +0300

    fix: another try

--- src/pages/Form/FormStepTwo/FormStepTwo.tsx
+++ src/pages/Form/FormStepTwo/FormStepTwo.tsx
@@ -34,7 +34,7 @@ const FormStepTwo: FC<FormStepTwoProps> = ({register, remove, append, fields}) =
               <div className={style.removeAdvantageContainer}>
                 <ButtonIcon
                   maxWidth="16px"
-                  picURL="icons/link-icon.svg"
+                  picURL="./form-test-task/icons/link-icon.svg"
                   onClick={() => remove(index)}
                   id={`button-remove-${index+1}`}
                 />
